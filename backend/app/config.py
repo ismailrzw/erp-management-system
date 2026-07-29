66459da7640bd77e791c6444f08d625ac529f6b0
Ismail Rizwan <ismailrana919@gmail.com>
2026-07-29 20:49:46 +0500

    chore: initial repo skeleton

--- backend/app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key')
-    MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017/pbl_system')
-    JWT_EXPIRATION_HOURS = int(os.getenv('JWT_EXPIRATION_HOURS', 24))
